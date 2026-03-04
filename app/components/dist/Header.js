ef1b664925a63c23d1b4094b1a2263dc9fbf3a26
Jake Mayores <jakemayores05@gmail.com>
2026-03-04 23:20:21 +0800

    add gsap animation

--- app/components/dist/Header.js
+++ app/components/dist/Header.js
@@ -62,7 +62,7 @@ function Header() {
         return function () { return window.removeEventListener("scroll", handleScroll); };
     }, []);
     var isActive = function (href) { return pathname === href; };
-    return (react_1["default"].createElement("header", { ref: headerRef, className: "bg-white border-b border-gray-200" },
+    return (react_1["default"].createElement("header", { ref: headerRef, className: "bg-white border-b border-gray-200 sticky top-0 z-50" },
         react_1["default"].createElement("div", { className: "max-w-7xl mx-auto px-4 sm:px-6" },
             react_1["default"].createElement("div", { className: "flex items-center justify-between h-16" },
                 react_1["default"].createElement("div", { className: "flex items-center" },
